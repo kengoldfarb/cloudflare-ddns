9020598118842acc65b3691da689f9e987720f85
Ken Goldfarb <hello@kengoldfarb.com>
2023-06-08 16:12:29 -0600

    config

--- release.config.js
+++ release.config.js
@@ -1,26 +1,13 @@
 const semanticReleaseConfig = require('@kengoldfarb/semantic-release-config')
 
 const config = semanticReleaseConfig.default({
-	config: {
-		app: {
-			branches: [
-				'main',
-				{ name: 'alpha', prerelease: true },
-				{ name: 'qa', prerelease: true },
-				{ name: 'dev', prerelease: true }
-			]
-		},
-		package: {
-			npmPublish: true,
-			branches: [
-				{ name: 'dev', channel: 'beta' },
-				{ name: 'canary', prerelease: true },
-				{ name: 'prerelease-*', prerelease: true }
-			],
-			// eslint-disable-next-line no-template-curly-in-string
-			releaseMessage: 'chore(release): ${nextRelease.version} [skip-ci-version]'
-		}
-	}
+	config: 'app',
+	branches: [
+		'main',
+		{ name: 'alpha', prerelease: true },
+		{ name: 'qa', prerelease: true },
+		{ name: 'dev', prerelease: true }
+	]
 })
 
 module.exports = config
